449ca47a75d25970dd3eef2055930edb9eca057a
burrows99 <raunakburrows@gmail.com>
2022-01-28 09:56:16 -0800

    Created MemeShare app

---
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.memeshare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var currentImageUrl : String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadMeme()
+    }
+    private fun loadMeme(){
+//        val textView = findViewById<TextView>(R.id.text)
+// ...
+
+// Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                currentImageUrl = response.getString("url")
+                Glide.with(this).load(currentImageUrl).into(memeImageView)},
+            Response.ErrorListener { Log.d("error",it.localizedMessage) })
+
+// Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+    }
+    fun shareMeme(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT,"Hey check this hilarious meme I found on reddit! $currentImageUrl")
+        val chooser = Intent.createChooser(intent,"Share this meme using...")
+        startActivity(chooser)
+    }
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+}
